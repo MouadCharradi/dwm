6396d521d1c50afcf65016ad3114467a231a91a8
Mouad Charradi <charradimouad58@gmail.com>
2023-01-15 17:28:11 +0100

    Configuring fonts and ModKey
    Signed-off-by: Mouad Charradi <charradimouad58@gmail.com>

--- config.def.h
+++ config.def.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 1;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "DejaVuMonoSpace:size=10" };
-static const char dmenufont[]       = "DejaVuMonoSpace:size=10";
+static const char *fonts[]          = { "DejaVuSansMono:size=12" };
+static const char dmenufont[]       = "DejaVuSansMono:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -46,7 +46,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
