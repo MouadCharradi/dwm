a0d9b863d089ecc26aa1bb4e586a754d622e906a
Mouad Charradi <charradimouad58@gmail.com>
2023-01-16 00:42:47 +0100

    Changed colors to match gruvbox theme
    Signed-off-by: Mouad Charradi <charradimouad58@gmail.com>

--- config.def.h
+++ config.def.h
@@ -8,11 +8,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "DejaVuSansMono:size=12" };
 static const char dmenufont[]       = "DejaVuSansMono:size=12";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#1d2021";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray4[]       = "#1d2021";
+static const char col_cyan[]        = "#83a598";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
